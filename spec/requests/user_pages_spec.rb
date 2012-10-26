213ea56dcd1ca9cb4f2fe5261aef7facd07dc2e0
Sean Yao <syao21@gmail.com>
2012-10-26 11:46:16 +0200

    Add users index

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,24 @@ describe "User pages" do
   
 	subject { page }
 
+	describe "index" do
+		before do
+			sign_in FactoryGirl.create(:user)
+			FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+			FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+			visit users_path
+		end
+
+		it { should have_selector('title', text: 'All Users') }
+		it { should have_selector('h1', text: 'All Users') }
+
+		it "should list each user" do
+			User.all.each do |user|
+				page.should have_selector('li', text: user.name)
+			end
+		end
+	end
+
 	describe "profile page" do
 		
 		let(:user) { FactoryGirl.create(:user) }
@@ -56,6 +74,44 @@ describe "User pages" do
 				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 				it { should have_link('Sign Out') }
 			end
+		end
+	end
+
+	describe "edit" do
+		let(:user) { FactoryGirl.create(:user) }
+		before do
+			sign_in user
+			visit edit_user_path(user)
+		end
+
+		describe "page" do
+			it { should have_selector('h1',		text: "Update Your Profile") }
+			it { should have_selector('title',	text: "Edit User") }
+			it { should have_link('Change', href: 'http://gravatar.com/emails') }
+		end
+
+		describe "with invalid information" do
+			before { click_button "Save Changes" }
+
+			it { should have_content('error') }
+		end
+
+		describe "with valid information" do
+			let(:new_name) 	{ "New Name" }
+			let(:new_email) { "new@example.com" }
+			before do
+				fill_in "Name",				with: new_name
+				fill_in "Email",			with: new_email
+				fill_in "Password",			with: user.password
+				fill_in "Confirm Password",	with: user.password
+				click_button "Save Changes"
+			end
+
+			it { should have_selector('title', text: new_name) }
+			it { should have_selector('div.alert.alert-success') }
+			it { should have_link('Sign Out', href: signout_path) }
+			specify { user.reload.name.should == new_name }
+			specify { user.reload.email.should == new_email }
 		end
 	end
 end
