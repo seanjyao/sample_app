36eb75ef9ba863e8a693d2152f6ffa21ba429ec2
Sean Yao <syao21@gmail.com>
2012-11-23 19:02:42 +0100

    Modify Relationship model

--- db/migrate/20121123171119_create_relationships.rb
+++ db/migrate/20121123171119_create_relationships.rb
@@ -1,6 +1,5 @@
 class CreateRelationships < ActiveRecord::Migration
   def change
-  	drop_table :relationships
     create_table :relationships do |t|
       t.integer :follower_id
       t.integer :followed_id
